7c81f1beead5e3af1508ce3746b1e0bac9725ff2
Priya Hiremath <priyahrish18@gmail.com>
2018-02-11 13:31:01 +1300

    name-address-project

---
+++ small-projects/web-form/name-address project/main.js
@@ -0,0 +1,6 @@
+function read(){
+  var name = document.getElementById("name").value;
+ console.log(name);
+   var address = document.getElementById("address").innerText; 
+   console.log(address);
+}
