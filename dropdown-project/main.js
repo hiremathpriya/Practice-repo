2de89c7e3d9f4b1f2cdc833668d769d543ab1923
Priya Hiremath <priyahrish18@gmail.com>
2018-02-12 10:57:36 +1300

    drop-down-project

---
+++ dropdown-project/main.js
@@ -0,0 +1,12 @@
+const dropDown = ()=> {
+    
+    
+  
+ document.getElementById("display1") 
+document.getElementById("myName").value
+    console.log(myName.value);
+document.getElementById("classId").value
+console.log(classId.value);
+console.log("am I working");
+
+}
