705d6d4cc89b230c7d9f6b54f54c71aad138bc84
Priya Hiremath <priyahrish18@gmail.com>
2018-02-12 13:56:51 +1300

    GetElementById-project

---
+++ getElementById/main.js
@@ -0,0 +1,6 @@
+const myFunction = ()=> {
+    document.getElementById("demo").innerHTML = "Hello World! I am practicing how to work with DOM events";
+    let x = document.getElementById("demo");
+    x.style.color = "blue";
+
+}
